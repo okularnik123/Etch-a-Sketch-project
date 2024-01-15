d56cc5c156183500296529b4cd20263e14d3ae3b
okularnik123 <okularnik7a@gmail.com>
2024-01-15 19:26:21 +0100

    Created input to edit grid size

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const containerDiv = document.querySelector('#container');
+const gridSizeInput = document.querySelector('#gridSizeInput');
 
 let gridSize = 16; 
+let paintingColor = "red";
 
+createGrid();
 
+gridSizeInput.addEventListener('change', () =>{
+    gridSize = gridSizeInput.value;
+    deleteGrid();
+    createGrid();
 
-for(let i = 1;i<=gridSize * gridSize; i++){
-    let gridDiv = document.createElement("div");
-    gridDiv.classList.add("gridDiv")
-    gridDiv.style.flexBasis = 600 / gridSize + "px";
-    gridDiv.style.minHeight = 600 / gridSize + 'px';
-    containerDiv.appendChild(gridDiv);
+});
+
+function createGrid(){
+    for(let i = 1;i <= gridSize * gridSize; i++){
+        let gridDiv = document.createElement("div");
+        gridDiv.classList.add("gridDiv")
+        gridDiv.style.flexBasis = 600 / gridSize + "px";
+        gridDiv.style.minHeight = 600 / gridSize + 'px';
+        containerDiv.appendChild(gridDiv);
+
+        gridDiv.addEventListener('mouseover', (e) =>{
+            e.target.style.backgroundColor = paintingColor;
+        });
+    }
+}
+function deleteGrid(){
+    while (containerDiv.firstChild){
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
 }
